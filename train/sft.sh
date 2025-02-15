a248c5d1f051641c536041fbaa743178e94a212b
BIMWERX <43457081+BimwerxNZ@users.noreply.github.com>
2025-02-15 15:18:08 +1300

    Update sft.sh

--- train/sft.sh
+++ train/sft.sh
@@ -35,7 +35,7 @@ torchrun --nproc-per-node ${gpu_count} --master_port 12345 \
     --adam_beta2=0.95 \
     --output_dir="s1-1" \
     --push_to_hub=${push_to_hub} \
-    --save_only_model=True
-    # --gradient_checkpointing=True \ Enable gradient checkpointing for efficient memory usage with 8 H100 GPUs.
+    --save_only_model=True \
+    --gradient_checkpointing=True
     # --accelerator_config='{"gradient_accumulation_kwargs": {"sync_each_batch": true}}'
 
